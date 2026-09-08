d3bc456bd0e1e8e4902d1dc2d165d2c97287853b
LeozinL3 <leo.jperez14@gmail.com>
2026-09-08 15:24:05 -0300

    Ataque(jogador); otimização (jogador, inimigo, entidade)

--- entidades.py
+++ entidades.py
@@ -1,23 +1,52 @@
 class Entidade:
-    def __init__(self, x, y, raio, vida):
+    def __init__(self, x, y, raio, vida, dano):
         self.x = x
         self.y = y
         self.raio = raio
-        
+
         self.vida = vida
         self.vida_max = vida
-        
-        
-        #recebido
+
+        # dano causado
+        self.dano = dano
+        self.cooldown_ataque = 0
+
+        # dano recebido
+        self.cooldown_receber_dano = 0
+
+        # visual do dano
         self.ultimo_dano = 0
-        self.tempo_dano = 0
-    
+        self.tempo_exibir_dano = 0
+
+
     def receber_dano(self, dano):
+
+        # ainda está invulnerável após o último golpe
+        if self.cooldown_receber_dano > 0:
+            return False
+
         self.vida -= dano
-        
+
         self.ultimo_dano = dano
-        self.tempo_dano = 20
-    
+        self.tempo_exibir_dano = 20
+
+        # frames de invulnerabilidade
+        self.cooldown_receber_dano = 10
+
+        return True
+
+
+    def atualizar_temporizadores(self):
+
+        if self.cooldown_ataque > 0:
+            self.cooldown_ataque -= 1
+
+        if self.cooldown_receber_dano > 0:
+            self.cooldown_receber_dano -= 1
+
+        if self.tempo_exibir_dano > 0:
+            self.tempo_exibir_dano -= 1
+
+
     def esta_vivo(self):
         return self.vida > 0
-        
--- jogador.py
+++ jogador.py
@@ -1,18 +1,18 @@
 import pyxel
 import math
 from entidades import Entidade
-from colisao import limitar_tela
+from colisao import colidiu, limitar_tela
 
         
 class Personagem(Entidade):
     def __init__(self, x, y, raio, cor):
-        super().__init__(x, y, raio, 100)
+        super().__init__(x, y, raio, 50, 3)
         self.cor = cor
-        self.velocidade_base = 1.75
+        self.velocidade_base = 1.5
         self.velocidade_dash = 6
 
-        self.dano = 10
         self.defesa = 0
+        self.cooldown_ataque_max = 20
         
         self.upgrades = {
             "espada": 0,
@@ -20,31 +20,60 @@ class Personagem(Entidade):
             "vida_extra": 0,
             "bota_celeridade": 0,
             "passo_sombrio": False,
-            "aura_espinhos": 0
+            "aura_espinhos": False
         }
-        
-        self.cooldown_dano = 0
         
         self.inventario = []
 
         self.dash = Dash()
         
-        
-    
-    def receber_dano(self, dano, inimigo=None):
-        # dash
-        if self.dash.invencivel:
+    def atacar(self, inimigos):
+
+        if self.cooldown_ataque > 0:
             return
 
+        if pyxel.btn(pyxel.KEY_SPACE):
+
+            for inimigo in inimigos:
+
+                if colidiu(self, inimigo, 10):
+
+                    dano_real = max(0, self.dano - inimigo.defesa)
+
+                    acertou = inimigo.receber_dano(dano_real)
+
+                    if acertou:
+                        self.cooldown_ataque = self.cooldown_ataque_max
+
+                    return
+    def draw_barra_ataque(self):
+        if self.cooldown_ataque > 0:
+         
+            largura = 6
+            altura = 5
+
+            progresso = 1 - (self.cooldown_ataque / self.cooldown_ataque_max)
+
+            pyxel.rect(self.x - largura//2 +10, self.y + 15,largura, altura,1)
+
+            pyxel.rect(self.x - largura//2 +10, self.y + 15, largura * progresso, altura, 14)
+        return
+    
+    
+    def receber_dano(self, dano, inimigo=None):
+        # Passo Sombrio
+        if self.dash.invencivel:
+            return False
+
+        # redução do dash
         if self.dash.reducao_dano:
             dano *= 0.3
             dano = int(dano)
-        
-        super().receber_dano(dano)
-        
-        
-        self.cooldown_dano = 10
 
+        recebeu = super().receber_dano(dano)
+
+        if not recebeu:
+            return False
 
         # aura de espinhos
         if self.upgrades["aura_espinhos"] and inimigo:
@@ -52,6 +81,8 @@ class Personagem(Entidade):
             dano_refletido = int(dano * 0.2)
 
             inimigo.receber_dano(dano_refletido)
+
+        return True
     
     
     def pegar_upgrade(self, upgrade):
@@ -63,11 +94,7 @@ class Personagem(Entidade):
 
     def update(self):
         #dano
-        if self.tempo_dano > 0:
-            self.tempo_dano -= 1
-        
-        if self.cooldown_dano > 0:
-            self.cooldown_dano -= 1
+        self.atualizar_temporizadores()
         
         
         # morte
@@ -76,16 +103,16 @@ class Personagem(Entidade):
         self.dx = 0
         self.dy = 0
 
-        if pyxel.btn(pyxel.KEY_RIGHT):
+        if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.KEY_D):
             self.dx += 1
 
-        if pyxel.btn(pyxel.KEY_LEFT):
+        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.KEY_A):
             self.dx -= 1
 
-        if pyxel.btn(pyxel.KEY_DOWN):
+        if pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.KEY_S):
             self.dy += 1
 
-        if pyxel.btn(pyxel.KEY_UP):
+        if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.KEY_W):
             self.dy -= 1
 
 
@@ -121,20 +148,20 @@ class Personagem(Entidade):
     def draw(self):
         # bola
         cor_atual = self.cor
-
+        
         # passo sombrio
         if self.upgrades["passo_sombrio"] and self.dash.ativo:
             cor_atual = 0
 
 
-        if self.cooldown_dano > 0 and not self.upgrades["passo_sombrio"]:
+        if self.cooldown_receber_dano > 0 and not self.upgrades["passo_sombrio"]:
             cor_atual = 13
 
         if self.dash.reducao_dano and not self.upgrades["passo_sombrio"]:
             cor_atual = 10
                 
         #dano
-        if self.tempo_dano > 0:
+        if self.tempo_exibir_dano > 0:
         
             pyxel.text(
                 self.x - 10,
@@ -155,13 +182,25 @@ class Personagem(Entidade):
         # espada
         if self.upgrades["espada"]:
 
-            pyxel.rect(
-                self.x + 7,
-                self.y - 15,
-                3,
-                20,
-                2
-            )
+            if self.cooldown_ataque > 0:
+                # atacando
+                pyxel.rect(
+                    self.x + 7,
+                    self.y + 1,
+                    20,
+                    3,
+                    2
+                )
+
+            else:
+                # normal
+                pyxel.rect(
+                    self.x + 7,
+                    self.y - 15,
+                    3,
+                    20,
+                    2
+                )
 
 
         # armadura
@@ -215,7 +254,8 @@ class Personagem(Entidade):
             7
         )
         # dash
-        self.dash.draw_barra(self.x, self.y)
+        self.dash.draw_barra_dash(self.x, self.y)
+        self.draw_barra_ataque()
         
         
 
@@ -228,7 +268,7 @@ class Dash:
         self.duracao = 10
         
         self.cooldown = 0
-        self.cooldown_max = 40
+        self.cooldown_max = 70
         
         self.reducao_dano = False
         self.invencivel = False
@@ -263,7 +303,7 @@ class Dash:
                 self.invencivel = False                    
  
 
-    def draw_barra(self, x, y):
+    def draw_barra_dash(self, x, y):
         if self.cooldown > 0:
          
             largura = 6
